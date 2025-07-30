ffe730a7bf19bbf3fa486f31735e54ebd560de1d
afzarrr <mochammadafdar@gmail.com>
2025-07-30 10:50:16 +0700

    menambahkan model clas

--- app/Models/Clas.php
+++ app/Models/Clas.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clas extends Model
 {
-    //
+    //nama tabel;
+    protected $table = "clases";
+
+    //fillabel;
+    protected $guarded = [];
+    
+    //relasi ke model user
+    public function user (){
+        return $this->hasMany(user::class, 'clas_id');
+    }
+
 }
